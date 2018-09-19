09d7776e13c86f2f16f64c2c46c064a1370fe4c6
da <>
2018-09-20 00:13:49 +0300

    Add implementation of advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+    let multipliers = [];
+    let num = 2;
+
+    while (num <= base) {
+        if (base % num === 0) {
+            let power = 0;
+
+            while (base % num === 0) {          
+                base /= num;
+                ++power;
+            }
+
+            multipliers.push({num: num, power: power});
+        }
+
+        ++num;
+    }
+  
+    let result = -1;
+
+    multipliers.forEach(multiplier => {
+        let count = 0;
+  
+        for(let divisor = multiplier.num; divisor < number; divisor *= multiplier.num) {
+            count += Math.floor(number / divisor);
+        }
+  
+        count = Math.floor(count / multiplier.power);
+  
+        if(count < result || result < 0) {
+            result = count;
+        }
+    });
+  
+    return result;
+  }
